73f72a37ae98a42b2237cc0cc7e341231402ff1c
sicilly <634068240@qq.com>
2019-12-11 19:23:54 +0800

    函数文档注释

--- 04_函数/04_第1个函数改造.py
+++ 04_函数/04_第1个函数改造.py
@@ -1,7 +1,11 @@
 name = "小明"
 
 # python 解释器知道下方定义了一个函数
+
+
 def say_hello():
+    """打招呼（我是函数的文档注释）"""
+
     print("hello 1")
     print("hello 2")
     print("hello 3")
@@ -9,7 +13,7 @@ def say_hello():
 
 print(name)
 
-# 只有在程序中主动调用函数，才会让函数执行
+# 只有在程序中主动调用函数，才会让函数执行 ctrl+Q查看函数文档注释
 say_hello()
 
 print(name)
