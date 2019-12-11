1a45cc41d66d6a3f7ceb386ce8bc533a15205787
sicilly <634068240@qq.com>
2019-12-11 21:59:17 +0800

    模块

---
+++ 04_函数/hm_10_体验模块.py
@@ -0,0 +1,4 @@
+import hm_10_分割线模块              # 导入
+
+hm_10_分割线模块.print_line("o",20)   # 函数
+print(hm_10_分割线模块.name)          # 变量
---
+++ 04_函数/hm_10_分割线模块.py
@@ -0,0 +1,16 @@
+def print_line(char, times):
+    print(char * times)
+
+
+def print_lines(char, times):
+    """ 打印多行分割线
+
+    :param char: 分割字符
+    :param times: 重复次数
+    """
+    row = 0
+    while row < 5:
+        print_line(char, times)
+        row += 1
+
+name = "sicilly"
