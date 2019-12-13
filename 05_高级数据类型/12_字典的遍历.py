864aef058eb7bd96890e0e5fa0115876598ba08e
sicilly <634068240@qq.com>
2019-12-13 23:37:41 +0800

    字典的遍历

---
+++ 05_高级数据类型/12_字典的遍历.py
@@ -0,0 +1,11 @@
+xiaoming = {"name": "小明",
+            "age": 18,
+            "gender": True,
+            "height": 1.75,
+            "weight": 75
+            }
+
+# 迭代遍历字典
+# 变量k是每一次循环中，获取到的键值对的key
+for k in xiaoming:
+    print(k, xiaoming[k])
