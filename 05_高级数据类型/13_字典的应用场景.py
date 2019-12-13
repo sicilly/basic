aeef124daf0ac67fd3230efa7bc411b554a77781
sicilly <634068240@qq.com>
2019-12-13 23:40:08 +0800

    字典的应用场景

---
+++ 05_高级数据类型/13_字典的应用场景.py
@@ -0,0 +1,11 @@
+card_list = [
+    {"name": "张三",
+     "qq": "12345",
+     "phone": "110"},
+    {"name": "里斯",
+     "qq": "54321",
+     "phone": "120"}
+]
+
+for card_info in card_list:
+    print(card_info)
