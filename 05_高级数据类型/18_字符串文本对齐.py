6ea61ce0d19af68b91cd42b46b084197f77c3828
sicilly <634068240@qq.com>
2019-12-14 23:33:31 +0800

    去除空白字符

--- 05_高级数据类型/18_字符串文本对齐.py
+++ 05_高级数据类型/18_字符串文本对齐.py
@@ -1,9 +1,11 @@
-poem = ["登鹳雀楼",
+poem = ["\t\n登鹳雀楼",
         "王之涣",
-        "白日依山尽",
+        "白日依山尽\t\n",
         "黄河入海流",
         "欲穷千里目",
         "更上一层楼"]
 
 for poem_str in poem:
-    print("|%s|" % poem_str.center(10," "))
+    # 先使用strip方法去除字符串中的空白字符
+    # 再使用center方法居中显示文本
+    print("|%s|" % poem_str.strip().center(10," "))
