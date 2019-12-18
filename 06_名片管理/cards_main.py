5aa7a2bd36ce250886ae8653cdc96445465cc933
sicilly <634068240@qq.com>
2019-12-18 21:05:22 +0800

    之前把main的代码写到tools文件里了，换回来

---
+++ 06_名片管理/cards_main.py
@@ -0,0 +1,32 @@
+# 无限循环，由用户主动决定什么时候退出循环
+while True:
+
+    # TODO(Sicilly) 显示功能菜单
+    action_str = input("请选择希望执行的操作：")
+    print("您选择的操作是【%s】" % action_str)
+
+    # 1,2,3 针对名片的操作
+    # 字符串判断
+    # 使用in针对列表判断，避免使用or拼接复杂的逻辑条件
+    # 没有使用int转换用户输入，可以避免一旦用户输入的不是数字，导致程序运行出错
+    if action_str in ["1", "2", "3"]:
+        # 新增名片
+        if action_str == "1":
+            pass
+        # 显示全部
+        elif action_str == "2":
+            pass
+        # 查询名片
+        elif action_str == "3":
+            pass
+        pass
+    # 0 退出系统
+    elif action_str == "0":
+        print("欢迎再次使用【名片管理系统】")
+        break
+        # 如果在开发程序时，不希望立刻编写分支内部的代码
+        # 可以使用pass关键字，表示一个占位符，能够保证程序的代码结构正确
+        # 程序运行时，pass关键字不会执行任何操作
+    # 其他内容 输入错误，需要提示用户
+    else:
+        print("您输入的不正确，请重新选择")
