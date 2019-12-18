dae231ae54df8a1aeb6ce47b610d69dcc071cf78
sicilly <634068240@qq.com>
2019-12-18 21:56:02 +0800

    显示名片

--- 06_名片管理/cards_tools.py
+++ 06_名片管理/cards_tools.py
@@ -46,6 +46,32 @@ def show_all():
     print("-" * 50)
     print("显示所有名片")
 
+    # 判断是否存在名片记录，如果没有，提示用户并且返回
+    if len(card_list) == 0:
+        print("当前没有任何的名片记录，请使用新增功能添加名片")
+
+        # return 可以返回一个函数的执行结果
+        # 下方的代码不会被执行
+        # 如果return后面没有任何内容，表示会返回到调用函数的位置
+        # 并且不返回任何的结果
+        return
+
+    # 打印表头
+    for name in ["姓名","电话", "QQ", "邮箱"]:
+        print(name, end="\t\t")
+
+    print("")
+
+    # 打印分割线
+    print("=" * 50)
+
+    # 遍历名片列表依次输出字典信息
+    for card_dict in card_list:
+        print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
+                                        card_dict["phone"],
+                                        card_dict["qq"],
+                                        card_dict["email"]))
+
 
 def search_card():
     """搜索名片"""
