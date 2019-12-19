e2f8eecdab9412ccda9664b804e864e1740697c3
sicilly <634068240@qq.com>
2019-12-19 16:09:16 +0800

    在开发时，应该把模块中所有全局变量定义在所有函数上方，就可以保证所有的函数都能够正常的访问到每一个全局变量了

--- 07_语法进阶/06_全局变量的位置.py
+++ 07_语法进阶/06_全局变量的位置.py
@@ -2,18 +2,17 @@
 # 定义在所有函数上方，就可以保证所有的函数
 # 都能够正常的访问到每一个全局变量了
 num = 10
+# 再定义一个全局变量
+title = "it"
+# 再定义一个全局变量 会报错
+name = "小明"
 
 
 def demo():
     print("%d" % num)
     print("%s" % title)
-    # print("%s" % name)
+    print("%s" % name)
 
-
-# 再定义一个全局变量
-title = "it"
 
 demo()
 
-# 再定义一个全局变量 会报错
-# name = "小明"
