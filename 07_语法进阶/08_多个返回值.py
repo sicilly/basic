6cf4a76b22edcf5be258a6f7b87deaec8f77e9f2
sicilly <634068240@qq.com>
2019-12-19 16:26:20 +0800

    元组 - 可以包含多个数据，因此可以使用元组让函数一次返回多个值

---
+++ 07_语法进阶/08_多个返回值.py
@@ -0,0 +1,15 @@
+def measure():
+    """测量温度和湿度"""
+
+    print("测量开始...")
+    temp = 39
+    wetness = 50
+    print("测量结束...")
+
+    # 元组 - 可以包含多个数据，因此可以使用元组让函数一次返回多个值
+    # 如果函数返回的类型是元组，小括号可以省略
+    return temp, wetness
+
+
+result = measure()
+print(result)
