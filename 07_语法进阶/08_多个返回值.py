72e1a7f665aee2281cccb9fc438005cecc19ddee
sicilly <634068240@qq.com>
2019-12-19 16:29:45 +0800

    如果函数返回的类型是元组，同时希望单独的处理元组中的元素 可以使用多个变量，一次接受函数的返回结果

--- 07_语法进阶/08_多个返回值.py
+++ 07_语法进阶/08_多个返回值.py
@@ -11,5 +11,18 @@ def measure():
     return temp, wetness
 
 
+# 元组
 result = measure()
 print(result)
+
+# 需要单独的处理温度和湿度 - 不方便
+print(result[0])
+print(result[1])
+
+
+# 如果函数返回的类型是元组，同时希望单独的处理元组中的元素
+# 可以使用多个变量，一次接受函数的返回结果
+# 注意：使用多个变量接受结果时，变量的个数应该和元组中元素的个数保持一致
+gl_temp, gl_wetness = measure()
+print(gl_temp)
+print(gl_wetness)
