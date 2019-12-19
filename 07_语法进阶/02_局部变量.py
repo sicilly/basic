54d1e827f60735f6237a07650530612f1afce884
sicilly <634068240@qq.com>
2019-12-19 13:38:21 +0800

    局部变量

---
+++ 07_语法进阶/02_局部变量.py
@@ -0,0 +1,15 @@
+def demo1():
+    # 定义一个局部变量
+    num = 10
+    print("在demo1函数内部的变量是 %d" % num)
+
+
+def demo2():
+    # print("%d" % num)
+    pass
+
+
+# 在函数内部定义的变量，不能在其他位置使用
+# print("%d" % num)
+demo1()
+demo2()
