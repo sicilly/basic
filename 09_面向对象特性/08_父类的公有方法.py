00b8d081a3caafa377e96dd36c3a12f76657f5ee
sicilly <634068240@qq.com>
2019-12-23 22:54:59 +0800

    子类对象可以通用父类的公有方法间接访问到私有属性或私有方法

--- 09_面向对象特性/08_父类的公有方法.py
+++ 09_面向对象特性/08_父类的公有方法.py
@@ -7,8 +7,10 @@ class A:
     def __test(self):
         print("私有方法 %d %d" % (self.num1, self.__num2))
 
+    # 子类对象可以通用父类的公有方法间接访问到私有属性或私有方法
     def test(self):
-        print("父类的公有方法")
+        print("父类的公有方法 %d" % self.__num2)
+        self.__test()
 
 
 class B(A):
